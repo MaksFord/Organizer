3cb25021c76621eafed776af537c6539a5448f5a
MaksFord <118970421+MaksFord@users.noreply.github.com>
2023-01-28 17:17:15 +0600

    Files changed to correct ones

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-const form = document.querySelector('form');
-const taskInput = document.getElementById('task-input');
-const taskList = document.getElementById('task-list');
+window.onload=function(){
+        document.getElementById("task-list").innerHTML=localStorage.getItem("MyList")
+}
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const task = taskInput.value;
+const form=document.getElementById("newTaskForm");
+const taskInput=document.getElementById("task-input");
+const taskList=document.getElementById("task-list");
 
-  const li = document.createElement('li');
-  li.textContent = task;
-  taskList.appendChild(li);
+form.addEventListener("submit", 
+(e)=>{
+        e.preventDefault()
+        const task=taskInput.value;
+        const li= document.createElement("li");
 
-  taskInput.value = '';
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.className="checkbox";
+        li.appendChild(checkbox);
+        
+        const label = document.createElement("label");
+        label.textContent = task;
+        li.appendChild(label);
+
+        const delButton= document.createElement("button");
+        delButton.className="delButton";
+        delButton.textContent="X";
+        delButton.addEventListener("click", function(){
+                let parent=delButton.parentNode;
+                parent.parentNode.removeChild(parent);
+        })
+        li.appendChild(delButton);
+
+        taskList.appendChild(li);
+
+        taskInput.value="";
+
+        localStorage.setItem("MyList",
+        document.getElementById("task-list").innerHTML);
 });
+
+
+
+
+function delLi (){
+
+}
+
+
+
+
+
+
+
