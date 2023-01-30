81d624de577cbf63571d7bb091c99e0f15b71729
MaksFord <118970421+MaksFord@users.noreply.github.com>
2023-01-31 00:32:39 +0600

    Main functionality is finished
    Date list added
    Function break implemented

--- script.js
+++ script.js
@@ -4,12 +4,49 @@ window.onload=function(){
         delButtons.forEach((delButton)=>{
                 delButton.addEventListener("click",(e)=>delParent(e))
         })
+        const cD = new Date();
+        const year = cD.getFullYear();
+        const month = (cD.getMonth() + 1).toString().padStart(2, '0');
+        const day = cD.getDate().toString().padStart(2, '0');
+        
+        const currentDate = `${year}-${month}-${day}`;
+        const taskListD=document.getElementById("task-listD");
+
+        var onDateTasks=JSON.parse(localStorage.getItem("onDateTasks")) || [];
+        onDateTasks.forEach(taskObject => {
+        let onDateTask = taskObject.onDateTask;
+        let dateOfTask = taskObject.dateOfTask;
+        if (currentDate===dateOfTask){
+                
+        const li= document.createElement("li");
+        
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.className="checkbox";
+        li.appendChild(checkbox);
+        
+        const label = document.createElement("label");
+        label.textContent = onDateTask;
+        li.appendChild(label);
+
+        const delButton= document.createElement("button");   //button deleting whole element on click
+        delButton.className="delButton";
+        delButton.textContent="X";
+        delButton.addEventListener("click", (e)=>delParent(e))
+        li.appendChild(delButton);
+        taskListD.appendChild(li);
+
+        }
+        else {alert (onDateTask+ " is planned for "+ dateOfTask+"so not yet")}
+              });
+        
 }
 
+//1st form and first list. Tasks are not saved in local storage but have a timer
 const form0=document.getElementById("oneSittingTaskForm");
 const taskInput0=document.getElementById("task-input0");
 const taskList0=document.getElementById("task-list0");
-const timeInput0=document.getElementById("time-input0")
+const timeInput0=document.getElementById("time-input0");
 
 form0.addEventListener("submit", 
 (e)=>{
@@ -26,25 +63,32 @@ form0.addEventListener("submit",
         label.textContent = task;
         li.appendChild(label);
 
-        const delButton= document.createElement("button");
+        const delButton= document.createElement("button");   //button deleting whole element on click
         delButton.className="delButton";
         delButton.textContent="X";
         delButton.addEventListener("click", (e)=>delParent(e))
         li.appendChild(delButton);
 
-//setting timers on task of first category
+//setting timers on tasks of first category
         const timer=timeInput0.value;
         const time=document.createElement("p");
         time.className="taskTimer"
         li.appendChild(time);
 
+
+
         let timeElapsed=0;
         const intervalId=setInterval(function() {
-                timeElapsed++;
+        if (checkbox.checked===false){
+        timeElapsed++;
+
         if (timer*60-timeElapsed>=0){
         time.innerHTML=`Time to do: ${timer*60-timeElapsed} seconds`}
         else{ li.className="timeIsUp";
-        timeElapsed.innerHTML="Time's Up"}
+        time.innerHTML="Time's Up"}}
+        else{
+        time.innerHTML=`Done in ${timeElapsed} seconds`
+        }
 
         }, 1000)
 
@@ -54,7 +98,7 @@ form0.addEventListener("submit",
         timeInput0.value="";
 });
 
-
+//2nd form, persistent tasks, saved in local storage, not lost on refresh
 const form=document.getElementById("everydayTaskForm");
 const taskInput=document.getElementById("task-input");
 const taskList=document.getElementById("task-list");
@@ -89,8 +133,6 @@ form.addEventListener("submit",
 });
 
 
-
-
 function delParent (event){
         let clicked=event.target;
         let parent=clicked.parentNode;
@@ -100,9 +142,62 @@ function delParent (event){
         document.getElementById("task-list").innerHTML);
 }
 
+//3rd form, tasks on date
+const formD=document.getElementById("onDateTaskForm");
+const taskInputD=document.getElementById("task-inputD");
+const taskListD=document.getElementById("task-listD");
+const dateInput=document.getElementById("date-input");
+
+const cD = new Date();
+const year = cD.getFullYear();
+const month = (cD.getMonth() + 1).toString().padStart(2, '0');
+const day = cD.getDate().toString().padStart(2, '0');
+
+const currentDate = `${year}-${month}-${day}`;
 
 
 
+formD.addEventListener("submit", 
+(e)=>{
+e.preventDefault()
+const task=taskInputD.value;
+alert (task)
+const taskDate=dateInput.value;
+alert (taskDate)
+if (taskDate>=currentDate){
+        var onDateTasks=JSON.parse(localStorage.getItem("onDateTasks")) || [];      
+        onDateTasks.push({
+                onDateTask:task,
+                dateOfTask:taskDate
+                });
+        // Save the updated onDateTasks array to local storage
+        localStorage.setItem("onDateTasks", JSON.stringify(onDateTasks));        
+if (currentDate===taskDate) {
+        const li= document.createElement("li");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.className="checkbox";
+        li.appendChild(checkbox);
+        
+        const label = document.createElement("label");
+        label.textContent = task;
+        li.appendChild(label);
+
+        const delButton= document.createElement("button");
+        delButton.className="delButton";
+        delButton.textContent="X";
+        delButton.addEventListener("click", (e)=>delParent(e))
+        li.appendChild(delButton);
+
+        taskListD.appendChild(li);
+
+        taskInputD.value="";
+        dateInput.value="";
+}
+}
+else {alert ("Enter correct date")}
+});
 
 
 
