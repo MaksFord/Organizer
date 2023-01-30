8161f5981525cd78ca2d3b04ee39fbab5f11d91f
MaksFord <118970421+MaksFord@users.noreply.github.com>
2023-01-30 12:12:12 +0600

    Need to do function break later
    Categories added
    Timers of first category added

--- script.js
+++ script.js
@@ -6,7 +6,56 @@ window.onload=function(){
         })
 }
 
-const form=document.getElementById("newTaskForm");
+const form0=document.getElementById("oneSittingTaskForm");
+const taskInput0=document.getElementById("task-input0");
+const taskList0=document.getElementById("task-list0");
+const timeInput0=document.getElementById("time-input0")
+
+form0.addEventListener("submit", 
+(e)=>{
+        e.preventDefault()
+        const task=taskInput0.value;
+        const li= document.createElement("li");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.className="checkbox";
+        li.appendChild(checkbox);
+        
+        const label = document.createElement("label");
+        label.textContent = task;
+        li.appendChild(label);
+
+        const delButton= document.createElement("button");
+        delButton.className="delButton";
+        delButton.textContent="X";
+        delButton.addEventListener("click", (e)=>delParent(e))
+        li.appendChild(delButton);
+
+//setting timers on task of first category
+        const timer=timeInput0.value;
+        const time=document.createElement("p");
+        time.className="taskTimer"
+        li.appendChild(time);
+
+        let timeElapsed=0;
+        const intervalId=setInterval(function() {
+                timeElapsed++;
+        if (timer*60-timeElapsed>=0){
+        time.innerHTML=`Time to do: ${timer*60-timeElapsed} seconds`}
+        else{ li.className="timeIsUp";
+        timeElapsed.innerHTML="Time's Up"}
+
+        }, 1000)
+
+        taskList0.appendChild(li);
+
+        taskInput0.value="";
+        timeInput0.value="";
+});
+
+
+const form=document.getElementById("everydayTaskForm");
 const taskInput=document.getElementById("task-input");
 const taskList=document.getElementById("task-list");
 
